1e7b8f67b942c508238c77aabc21841a27e849e6
DavidTurkGT <dlturk747@gmail.com>
2017-07-16 15:58:48 -0400

    Added more comprehensive test of users

--- server/test/users.js
+++ server/test/users.js
@@ -7,7 +7,17 @@ const Models  = require('../models');
 let User;
 
 before("Starting up...", (done) => {
-  done();
+  let newUser = {
+    username: "test",
+    salt: "himalayan",
+    iterations: 42,
+    hash: "browns"
+  }
+  Models.Users.create(newUser)
+  .then( (user) => {
+    User = user;
+    done();
+  });
 });
 
 after("Cleaning up...", (done) => {
@@ -29,7 +39,6 @@ describe("A user", () => {
         assert(receivedUser.password.salt, "Received password has no salt");
         assert(receivedUser.password.iterations, "Received password has no iterations");
         assert(receivedUser.password.hash, "Received password has no hash");
-        User = receivedUser;
       })
       .end( (err, res) => {
         if(err) done(err);
@@ -37,15 +46,47 @@ describe("A user", () => {
       })
   });
 
+  it("will throw a 400 if username is blank", (done) => {
+    request(Server)
+      .post("/api/users")
+      .send({username: "", password: "fail"})
+      .expect(400)
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      });
+  });
+
+  it("will throw a 400 if password is blank", (done) => {
+    request(Server)
+      .post('/api/users')
+      .send({username:'noPW', password: ""})
+      .expect(400)
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      });
+  });
+
+  it("will throw a 400 if username is taken", (done) => {
+    request(Server)
+      .post('/api/users')
+      .send({username: 'test', password: 'taken'})
+      .expect(400)
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      })
+  })
+
   it("can be retrieved by ID", (done) => {
     request(Server)
-      .get('api/users/'+User.id)
+      .get('/api/users/'+User.id)
       .expect(200)
       .expect('Content-Type','application/json; charset=utf-8')
       .expect( (res) => {
         assert(res.body.user, "No user sent back");
-        let receivedUser = res.body.user;
-        assert.equal(receivedUser.id, User.id, "Wrong user retrieved. Expected ID#: " + User.id + ". Received: " + receivedUser.id);
+        assert.equal(res.body.user.id, User.id, "Id's do not match. Expected: " + User.id + ". Received: " + res.body.user.id);
       })
       .end( (err, res) => {
         if(err) done(err);
@@ -66,7 +107,6 @@ describe("A user", () => {
       .end( (err, res) => {
         if(err) done(err);
         else done();
-      })
-    done();
+      });
   });
 });
