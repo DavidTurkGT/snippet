def4a3425585bfd789b8f867dccf23fef1da64f1
DavidTurkGT <dlturk747@gmail.com>
2017-07-16 13:41:48 -0400

    Added API router and routes

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,23 +3,27 @@ const router  =             express.Router();
 
 router.get('/', (req, res) => {
   //Returns an array of all users
-
+  res.send("Returns an array of all users");
 });
 
 router.post("/", (req, res) => {
   //Create a new user
+  res.send("Create a new user");
 });
 
 router.get('/:userId', (req, res) => {
   //Find a specific user
+  res.send("Find a specific user");
 });
 
 router.post('/login', (req, res) => {
   //Logs a user in
+  res.send("Log in a user");
 });
 
 router.get('/whoami', (req, res) => {
   //Returns the user on the session or null
+  res.send("Returns the session");
 });
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
 const express           = require('express');
 const router            = express.Router();
+const ApiRouter         = require('./api');
+
+router.use("/api/", ApiRouter);
 
 router.get("/", (req, res) => res.send("Hello there!") );
 
