9c0348156c2fa793cb36b2861eabb5778cc00fcf
DavidTurkGT <dlturk747@gmail.com>
2017-07-17 13:51:14 -0400

    Added sign up capabilities to front-end

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,6 @@ function signup() {
     });
   }
   else{
-    //TODO sign up a new user
     let newUser = createNewUser();
     let url = '/api/users';
     let request = new Request(url, {
@@ -35,13 +34,22 @@ function signup() {
     fetch(request)
     .then( (res) => {
       if(res.status !== 200){
-        let msgContainer = document.querySelector(".messages");
-        let p = document.createElement("p");
-        p.textContent = "Error signing up";
-        msgContainer.appendChild(p);
+        res.json().then( (errors) => {
+          let msgContainer = document.querySelector(".messages");
+          clearChildren(msgContainer);
+          let p = document.createElement("p");
+          p.textContent = errors.msg;
+          msgContainer.appendChild(p);
+        })
       }
       else{
-        // window.location.replace()
+        res.json()
+        .then( (data) => {
+          let msgContainer = document.querySelector(".messages");
+          let p = document.createElement("p");
+          p.textContent = "Created user: " + data.user.username;
+          msgContainer.appendChild(p);
+        })
       }
     })
   }
@@ -52,14 +60,12 @@ function signUpValidationErrors() {
 
   let usernameInput = document.querySelector('input[name="username-sign-up"]');
   let username = usernameInput.value;
-  usernameInput.value = "";
   if(username === ""){
     errMsg.push("Username cannot be blank");
   }
 
   let passwordInput = document.querySelector('input[name="password-sign-up"]');
   let password = passwordInput.value;
-  passwordInput.value = "";
   if(password === "") {
     errMsg.push("Password cannot be blank");
   }
@@ -84,9 +90,11 @@ function clearChildren(element){
 function createNewUser(){
   let usernameInput = document.querySelector("input[name='username-sign-up']");
   let username = usernameInput.value;
+  usernameInput.value = "";
 
   let passwordInput = document.querySelector("input[name='password-sign-up']");
   let password = passwordInput.value;
+  passwordInput.value = "";
 
   return newUser = {
     username: username,
