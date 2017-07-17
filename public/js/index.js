29e55b3894545cb6e44b02c78238016c7ee62901
DavidTurkGT <dlturk747@gmail.com>
2017-07-17 13:59:17 -0400

    Added log in capabilities to the front-end

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,52 @@ console.log("Hello there!");
 
 //TODO:Integrate with API to log in and create users
 function login() {
-  console.log("You want to log in");
+  let errors = logInValidationErrors();
+  if(errors.length){
+    let msgContainer = document.querySelector(".messages");
+    clearChildren(msgContainer);
+    errors.forEach( (error) => {
+      let p = document.createElement("p");
+      p.textContent = error;
+      msgContainer.appendChild(p);
+    });
+  }
+  else{
+    let usernameInput = document.querySelector("input[name='username-login']");
+    let username = usernameInput.value;
+
+    let passwordInput = document.querySelector("input[name='password-login']");
+    let password = passwordInput.value;
+
+    let url = '/api/users/login';
+    let request = new Request(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: username,
+        password: password
+      }),
+      headers: new Headers({
+        'Content-Type':'application/json'
+      }),
+      redirect: 'follow'
+    });
+
+    fetch(request)
+    .then( (res) => {
+      if(res.status !== 200){
+        res.json().then( (error) => {
+          let msgContainer = document.querySelector(".messages");
+          clearChildren(msgContainer);
+          let p = document.createElement("p");
+          p.textContent = error.msg;
+          msgContainer.appendChild(p);
+        })
+      }
+      else{
+        window.location.replace("/home");
+      }
+    })
+  }
 };
 
 function signup() {
@@ -80,6 +125,24 @@ function signUpValidationErrors() {
   return errMsg === [] ? null : errMsg;
 
 };
+
+function logInValidationErrors() {
+  let errMsg = [];
+
+  let usernameInput = document.querySelector('input[name="username-login"]');
+  let username = usernameInput.value;
+  if(username === ""){
+    errMsg.push("Username cannot be blank");
+  }
+
+  let passwordInput = document.querySelector('input[name="password-login"]');
+  let password = passwordInput.value;
+  if(password === "") {
+    errMsg.push("Password cannot be blank");
+  }
+
+  return errMsg === [] ? null : errMsg;
+}
 
 function clearChildren(element){
   while(element.hasChildNodes()){
