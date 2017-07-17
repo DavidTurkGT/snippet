b9e9206022499f40a925c5c1bb65b1d9c52b4874
DavidTurkGT <dlturk747@gmail.com>
2017-07-17 15:00:28 -0400

    Added route to create new snippet

--- routes/api/snippets.js
+++ routes/api/snippets.js
@@ -6,6 +6,10 @@ router.get("/", (req, res) => {
   res.send("Return an array of all snips");
 });
 
+router.post('/', (req, res) => {
+  res.send("Create a new code snip");
+})
+
 router.get("/:userId", (req, res) => {
   res.send("Return only an array of snips for specified user");
 });
